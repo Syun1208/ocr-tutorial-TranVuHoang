16802dde0be86ec45535428c7bd3ae07a82e3e69
Syun1208 <phamlong12082001@gmail.com>
2023-11-14 22:54:13 +0700

    Configure path of the pretrained models

--- utils/CRNN/get_pretrained_model.sh
+++ utils/CRNN/get_pretrained_model.sh
@@ -1,3 +1,4 @@
 mkdir ./pths
 cd pths/
+# get pretrained CRNN model in English with 100 epochs
 gdown --id 17qphAZyS2SV-oygcVn2CCOy_Nk4y1vGY
--- utils/EAST/get_pretrained_model.sh
+++ utils/EAST/get_pretrained_model.sh
@@ -2,5 +2,5 @@ mkdir pths/
 cd pths/
 # pretrained chinese model
 gdown --id 1HgDuFGd2q77Z6DcUlDEfBZgxeJv4tald
-# pretrained english model with 80 epochs
-gdown --id 1ZBGSUHmrnI3EQycHxyBqHqGIVhBufRne
+# pretrained english model with 100 epochs
+gdown --id 11k-E6UjrLkVxG9Y9Kt2d15uUhcmWE7rg
